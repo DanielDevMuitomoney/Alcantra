56464f672c8fe73a98a1e804921422cdb4d058a3
Daniel Alvarez de Almeida <skyrimdaniel11@gmail.com>
2022-10-12 01:16:14 -0300

    Home page,card component

--- Alcantra-App/resources/views/internal/home.blade.php
+++ Alcantra-App/resources/views/internal/home.blade.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="styles/layouts_css/lay_logged.css">
     <title>Document</title>
 </head>
 <body>
-    home
+    @extends('layouts.lay_logged')
+
+    @section('content')
+    <x-card title="2"/>
+    @endsection
 </body>
 </html>
---
+++ Alcantra-App/resources/views/layouts/lay_logged.blade.php
@@ -0,0 +1,16 @@
+<header >
+    <nav class="menu">
+        <div>
+        <i id="list-menu" class="bi bi-list-ul"></i>
+        </div>
+        <div>
+            <input type="search" name="search" id="search">
+            <i class="bi bi-search"></i>
+        </div>
+        <img src="/img-ideias/user-icon.jpg" alt="imagem da logo" id="img-user">
+        
+    </nav>
+</header>
+
+
+@yield('content')
